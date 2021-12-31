07e2f18638a3fed317b427d52aaac503bafde9e2
EddyGiusepe <eddychirinos.unac@gmail.com>
2021-12-31 00:26:51 -0300

    Script para consumir API

--- consumirminha_API.py
+++ consumirminha_API.py
@@ -1,5 +1,13 @@
 '''
 Data Scientist Jr.: Dr.Eddy Giusepe Chirinos Isidro
 O link de estudo é: https://www.youtube.com/watch?v=WWVEymSt1iI&t=1417s
+Estou usando: https://replit.com/@EddyGiusepe/MinhaAPI#main.py
 '''
-
+import requests
+link = "https://minhaapi.eddygiusepe.repl.co/pegarvendas"
+requisicao = requests.get(link)
+print(requisicao) # 200 significa
+print(requisicao.json())
+# Também podemos fazer:
+dicionario = requisicao.json()
+print(dicionario["total_vendas"])
